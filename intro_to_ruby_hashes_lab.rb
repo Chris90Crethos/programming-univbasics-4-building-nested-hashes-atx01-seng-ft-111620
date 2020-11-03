1b983bfb800ad7d5601687f0246071b02f1a6951
Chris Martin <bmg.martin08@gmail.com>
2020-11-03 21:37:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,9 +36,14 @@ def monopoly_with_third_tier
       :pieces => 4,
       :rent_in_dollars => 
       {
-        
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 500,
       },
-      :names => {}
+      :names => {
+        
+      }
     }
   }
 end
